16c8060012981efbcd8b5a9558e898cbdcd407cb
Aleksandr Vyun <cifrotech@yandex.ru>
2022-09-06 10:57:42 +1000

    upload gltf model

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ scene = new THREE.Scene();
 scene.background = new THREE.Color(0xFFE9D1);
 
 camera = new THREE.PerspectiveCamera(50, window.innerWidth/window.innerHeight);
-camera.position.set(0,100,1000);
+camera.position.set(0,-3,17);
 
 renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -12,6 +12,19 @@ document.body.appendChild(renderer.domElement);
 
 light = new THREE.AmbientLight(0xF0EAD6);
 scene.add(light);
+
+window.addEventListener('resize', function() {
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	camera.aspect = window.innerWidth / window.innerHeight;
+})
+
+/// GLTF loader
+
+let loader = new THREE.GLTFLoader();
+
+loader.load('../models/scene.gltf', function(gltf) {
+	scene.add(gltf.scene);
+})
 
 function animate() {
 	requestAnimationFrame(animate);
