df0f1d19eb6986a8214ad640ee8f002f2266d785
Aleksandr Vyun <cifrotech@yandex.ru>
2022-09-06 11:04:29 +1000

    complete

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
-let scene, camera, renderer, light;
+let scene, camera, renderer, light,
+	loader, controls;
 
 scene = new THREE.Scene();
-scene.background = new THREE.Color(0xFFE9D1);
+scene.background = new THREE.Color(0xCC99FF);
 
 camera = new THREE.PerspectiveCamera(50, window.innerWidth/window.innerHeight);
-camera.position.set(0,-3,17);
+camera.position.set(0,0,25);
 
 renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-light = new THREE.AmbientLight(0xF0EAD6);
+light = new THREE.AmbientLight(0xF0EAD6,1);
 scene.add(light);
 
 window.addEventListener('resize', function() {
@@ -18,9 +19,14 @@ window.addEventListener('resize', function() {
 	camera.aspect = window.innerWidth / window.innerHeight;
 })
 
+/// controls
+
+controls = new THREE.OrbitControls(camera, renderer.domElement);
+controls.update();
+
 /// GLTF loader
 
-let loader = new THREE.GLTFLoader();
+loader = new THREE.GLTFLoader();
 
 loader.load('../models/scene.gltf', function(gltf) {
 	scene.add(gltf.scene);
